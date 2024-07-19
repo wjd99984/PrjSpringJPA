bea535a2348f66af5cfa23c73e5e13f67167f241
jung tae wook <xodnr901901@naver.com>
2024-07-19 09:13:51 +0900

    Fist : 첫번째 커밋

---
+++ src/main/java/com/prjjpa/PrjJpaApplication.java
@@ -0,0 +1,13 @@
+package com.prjjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrjJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prjjpa/PrjJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prjjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
